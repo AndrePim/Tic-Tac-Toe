d886cd5ec9cd24a82388f3227a654b48d59022da
AndrePim <andreo.obolensky@gmail.com>
2022-10-29 22:39:46 +0300

    add play RandomComputer and GeniusComputer with MiniMax

--- game.py
+++ game.py
@@ -117,12 +117,6 @@ def play(game, x_player, o_player, print_game=True):
         print('It\'s a tie!')
 
 
-# if __name__ == '__main__':
-#     x_player = HumanPlayer('X')
-#     o_player = GeniusComputerPlayer('O')
-#     t = TicTacToe()
-#     play(t, x_player, o_player, print_game=True)
-
 if __name__ == '__main__':
     x_player = RandomComputerPlayer('X')
     o_player = GeniusComputerPlayer('O')
